be959e6ddc35dbf618fac8293d730160182d8f81
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-10-08 13:05:21 -0400

    Before code review

---
+++ quotes.js
@@ -0,0 +1,219 @@
+const localQuotes = [
+  {
+    text: "Today is the tomorrow we worried about yesterday.",
+    author: "Anonymous",
+  },
+  {
+    text: "It's easier to see the mistakes on someone else's paper.",
+    author: "Anonymous",
+  },
+  {
+    text: "Every man dies. Not every man really lives.",
+    author: "Anonymous",
+  },
+  {
+    text: "The day is already blessed, find peace within it.",
+    author: "Anonymous",
+  },
+  {
+    text: "From small beginnings come great things.",
+    author: "Anonymous",
+  },
+  {
+    text: "It takes both sunshine and rain to make a rainbow.",
+    author: "Anonymous",
+  },
+  {
+    text: "A beautiful thing is never perfect.",
+    author: "Anonymous",
+  },
+  {
+    text: "Every new day is another chance to change your life.",
+    author: "Anonymous",
+  },
+  {
+    text: "Some pursue happiness, others create it.",
+    author: "Anonymous",
+  },
+  {
+    text: "We do what we do because we believe.",
+    author: "Anonymous",
+  },
+  {
+    text: "The harder you fall, the higher you bounce.",
+    author: "Anonymous",
+  },
+  {
+    text: "Everyone smiles in the same language.",
+    author: "Anonymousk",
+    tag: "",
+  },
+  {
+    text: "You can do what's reasonable or you can decide what's possible.",
+    author: "Anonymous",
+  },
+  {
+    text: "Friendship isn't a big thing. It's a million little things.",
+    author: "Anonymous",
+  },
+  {
+    text: "A good plan today is better than a perfect plan tomorrow.",
+    author: "Anonymous",
+  },
+  {
+    text: "Someone remembers, someone cares; your name is whispered in someone's prayers.",
+    author: "Anonymous",
+  },
+  {
+    text: "Don't be pushed by your problems; be led by your dreams.",
+    author: "Anonymous",
+  },
+  {
+    text: "Change your thoughts, change your life!",
+    author: "Anonymous",
+  },
+  {
+    text: "Today is the tomorrow you worried about yesterday.",
+    author: "Anonymous",
+  },
+  {
+    text: "The day always looks brighter from behind a smile.",
+    author: "Anonymous",
+  },
+  {
+    text: "A stumble may prevent a fall.",
+    author: "Anonymous",
+  },
+  {
+    text: "Put your future in good hands your own.",
+    author: "Anonymous",
+  },
+  {
+    text: "What you see depends on what you're looking for.",
+    author: "Anonymous",
+  },
+  {
+    text: "Worry gives a small thing a big shadow.",
+    author: "Anonymous",
+  },
+  {
+    text: "To get something you never had, you have to do something you never did.",
+    author: "Anonymous",
+  },
+  {
+    text: "Be thankful when you don't know something for it gives you the opportunity to learn.",
+    author: "Anonymous",
+  },
+  {
+    text: "Letting go is not the end of the world; it is the beginning of a new life.",
+    author: "Anonymous",
+  },
+  {
+    text: "Our greatest glory is not in never failing but rising everytime we fall.",
+    author: "Anonymous",
+  },
+  {
+    text: "Being right is highly overrated. Even a stopped clock is right twice a day.",
+    author: "Anonymous",
+  },
+  {
+    text: "Love is not blind; it simply enables one to see things others fail to see.",
+    author: "Anonymous",
+  },
+  {
+    text: "If you get up one more time than you fall, you will make it through.",
+    author: "Anonymous",
+  },
+  {
+    text: "Don't focus on making the right decision, focus on making the decision the right one.",
+    author: "Anonymous",
+  },
+  {
+    text: "Love is just a word until someone comes along and gives it meaning.",
+    author: "Anonymous",
+  },
+  {
+    text: "We all have problems. The way we solve them is what makes us different.",
+    author: "Anonymous",
+  },
+  {
+    text: "Invent your world. Surround yourself with people, color, sounds, and work that nourish you.",
+    author: "Anonymous",
+  },
+  {
+    text: "Every day may not be good, but there's something good in every day.",
+    author: "Anonymous",
+  },
+  {
+    text: "Some people think it's holding that makes one strong sometimes it's letting go.",
+    author: "Anonymous",
+  },
+  {
+    text: "Why worry about things you cannot control when you can keep yourself busy controlling the things that depend on you?",
+    author: "Anonymous",
+  },
+  {
+    text: "It is better to take many small steps in the right direction than to make a great leap forward only to stumble backward.",
+    author: "Anonymous",
+  },
+  {
+    text: "As the rest of the world is walking out the door, your best friends are the ones walking in.",
+    author: "Anonymous",
+  },
+  {
+    text: "Why compare yourself with others? No one in the entire world can do a better job of being you than you.",
+    author: "Anonymous",
+  },
+  {
+    text: "A good teacher is like a candle it consumes itself to light the way for others.",
+    author: "Anonymous",
+  },
+  {
+    text: "Life is not measured by the breaths we take, but by the moments that take our breath.",
+    author: "Anonymous",
+  },
+  {
+    text: "The real measure of your wealth is how much youd be worth if you lost all your money.",
+    author: "Anonymous",
+  },
+  {
+    text: "Though no one can go back and make a brand new start, anyone can start from now and make a brand new ending.",
+    author: "Anonymous",
+  },
+  {
+    text: "A friend is someone who understands your past, believes in your future, and accepts you just the way you are.",
+    author: "Anonymous",
+  },
+  {
+    text: "Giving up doesn't always mean you are weak. Sometimes it means that you are strong enough to let go.",
+    author: "Anonymous",
+  },
+  {
+    text: "Never miss an opportunity to make others happy, even if you have to leave them alone in order to do it.",
+    author: "Anonymous",
+  },
+  {
+    text: "Yesterday is history. Tomorrow is a mystery. And today? Today is a gift that's why they call it the present.",
+    author: "Anonymous",
+  },
+  {
+    text: "When you don't know what you believe, everything becomes an argument. Everything is debatable. But when you stand for something, decisions are obvious.",
+    author: "Anonymous",
+  },
+  {
+    text: "Every sixty seconds you spend angry, upset or mad, is a full minute of happiness you will never get back.",
+    author: "Anonymous",
+  },
+  {
+    text: "If we are facing in the right direction, all we have to do is keep on walking.",
+    author: "Anonymous",
+  },
+  {
+    text: "Peace of mind is not the absence of conflict from life, but the ability to cope with it.",
+    author: "Anonymous",
+  },
+  {
+    text: "An obstacle may be either a stepping stone or a stumbling block.",
+    author: "Anonymous",
+  },
+];
